4ba1e1fe72067833fa493236b70f0d346587e776
elefteroff <elefteroff@mac.com>
2021-08-28 15:45:18 -0700

    Initial Commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,34 @@
+#Basic:
+for x in range(0, 151):
+    print(x)
+
+#Multiples of Five:
+for x in range(5, 1001, 5):
+    print(x)
+
+#Counting, the Dojo Way:
+for x in range(1,101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
+
+#Whoa. That Sucker's Huge:
+sum = 0
+for x in range(1,500000):
+    if x % 2 != 0:
+        sum = sum + x
+        print(sum)
+
+#Countdown by Fours:
+for x in range(2018,0,-4):
+    print(x)
+
+#Flexible Counter:
+lowNum = 20
+highNum = 40
+mult = 2
+for x in range(lowNum, highNum+1,2):
+    print(x)
